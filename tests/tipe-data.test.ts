e6598a7a4d519ae961cf3d7a11b7fc5bad8bac8b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-17 14:44:15 +0700

    tipe data

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,12 @@
+describe("Data-Type", function () {
+    it("should must declare", function () {
+        const name: string = "Rama Fajar Fadhillah";
+        const balance: number = 1000000;
+        const isVIP: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVIP);
+
+    })
+})
