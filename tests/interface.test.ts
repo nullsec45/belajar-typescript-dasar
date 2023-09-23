5d99e641f284ec753ab3b02835764b7cd152bff0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-23 13:41:26 +0700

    extended interace & support function in interface test

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,4 +1,5 @@
 import { Seller } from "../src/seller"
+import { Employee, Manager } from "../src/employee";
 
 describe("interface", function () {
     it("should support in typescript", function () {
@@ -8,5 +9,40 @@ describe("interface", function () {
         }
 
         console.info(seller)
+    });
+
+    it("should support extends interface", function () {
+        const employee: Employee = {
+            id: "1",
+            name: "Fajar",
+            division: "IT"
+        }
+
+        console.info(employee);
+
+        const manager: Manager = {
+            id: "2",
+            name: "Fadhillah",
+            division: "IT",
+            numberOfEmployees: 10
+        }
+
+        console.info(manager);
+    });
+
+    it("should support function in interface", function () {
+        interface Person {
+            name: string,
+            sayHello(name: string): string;
+        }
+
+        const person: Person = {
+            name: "Fajar",
+            sayHello: function (name: string): string {
+                return `Hello ${name}, my name is ${this.name}`;
+            }
+        }
+
+        console.info(person.sayHello("Gita"));
     })
 })
