6e667586eb1f0b6c943dd5c32b542fa18f4b7ad8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-27 23:52:08 +0700

    function test

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -1,13 +1,29 @@
-describe("function", function () {
-    it("should support function interface", function () {
-        interface AddFunction {
-            (value1: number, value2: number): number;
+import { sayHello } from "../src/say-hello";
+
+describe("test function", function () {
+    it("should support function in typescript", function () {
+        expect(sayHello("Denisa")).toBe("Hello Denisa");
+
+        function printHello(name: string): void {
+            console.info(`Hello ${name}`);
         }
 
-        const add: AddFunction = (value1: number, value2: number): number => {
-            return value1 + value2;
+        printHello("Gita");
+    });
+
+    it("should default value value", function () {
+        function goodMorning(name: string = "Guest"): string {
+            return `Good Morning ${name}`;
+        }
+        expect(goodMorning()).toBe("Good Morning Guest");
+        expect(goodMorning("Fajar")).toBe("Good Morning Fajar");
+    });
+
+    it("should support rest parameter", function () {
+        function sum(...values: number[]): number {
+            return values.reduce((acc, cur) => acc + cur);
         }
 
-        expect(add(2, 2)).toBe(4);
+        expect(sum(1, 2, 3, 4, 5)).toBe(15);
     })
 })
